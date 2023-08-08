02df97b35ffd257aa02d82fbd2aac516484cf708
Mio Arai <mio311a@gmail.com>
2023-08-08 09:20:28 -0400

    Vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link class="btn" v-bind:to="{ name: 'home' }" ><i class="fa fa-home"></i> Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
@@ -10,6 +11,24 @@
 
 <style scoped>
 #nav {
-  background-color: gold;
+  background-color: pink;
+}
+.btn{
+  background-color: rgb(252, 142, 160);
+  border: none; /* Remove borders */
+  color: white; /* White text */
+  padding: 12px 16px; /* Some padding */
+  font-size: 16px; /* Set a font size */
+  cursor: pointer; /* Mouse pointer on hover */
+  text-decoration: none;
+  border-radius: 25px;
+}
+
+.btn:hover {
+   top: 3px;
+  background-color:#e83e8c;
+  -webkit-box-shadow: none; 
+  -moz-box-shadow: none; 
+  box-shadow: none;
 }
 </style>
