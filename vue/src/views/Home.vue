777e2537495f334b44e052f31a75211df53d2c69
Hayden Slyk <hslyk9923@gmail.com>
2023-08-09 09:44:20 -0400

    CSS Background Colors

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -21,7 +21,7 @@ export default {
 
 <style>
 .home {
-  background-color: pink;
+  background-image: radial-gradient(rgb(255, 225, 230), rgb(255, 171, 185)) ;
   height: 100vh;
   width: 100vw;
   display: grid;
