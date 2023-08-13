e3f3cea7e8b23cdfc9312ca13b103268e3974dc8
Mio Arai <mio311a@gmail.com>
2023-08-13 11:06:54 -0400

    sunday

--- java/src/main/java/com/techelevator/controller/FollowController.java
+++ java/src/main/java/com/techelevator/controller/FollowController.java
@@ -1,37 +1,37 @@
-package com.techelevator.controller;
-
-import java.security.Principal;
-import java.util.List;
-
-@RestController
-@CrossOrigin(origins="*")
-@PreAuthorize("isAuthenticated()")
-
-public class FollowController {
-    @Autowired
-    private UserDAO userDAO;
-    @Autowired
-    private FollowDAO followDAO;
-
-    // follow another user  -- post
-    @ResponseStatus(HttpStatus.CREATED)
-    @RequestMapping(path = "/follow/{followedId}", method = RequestMethod.POST)
-    public void follow(@PathVariable int followedId, Principal principal) {
-        int followerId = userDAO.findIdByUsername(principal.getName());
-        followDAO.follow(followerId, followedId);
-    }
-
-    // delete followed user
-    @RequestMapping(path = "/follow/{followedId}", method = RequestMethod.DELETE)
-    public void unfollow(@PathVariable int followedId, Principal principal) {
-        int followerId = userDAO.findIdByUsername(principal.getName());
-        followDAO.unfollow(followerId, followedId);
-    }
-
-    // get list of followed users
-    @RequestMapping(path = "/follow", method = RequestMethod.GET)
-    public List<User> list(Principal principal) {
-        int userId = userDAO.findIdByUsername(principal.getName());
-        return followDAO.list(userId);
-    }
-}
+//package com.techelevator.controller;
+//
+//import java.security.Principal;
+//import java.util.List;
+//
+//@RestController
+//@CrossOrigin(origins="*")
+//@PreAuthorize("isAuthenticated()")
+//
+//public class FollowController {
+//    @Autowired
+//    private UserDAO userDAO;
+//    @Autowired
+//    private FollowDAO followDAO;
+//
+//    // follow another user  -- post
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @RequestMapping(path = "/follow/{followedId}", method = RequestMethod.POST)
+//    public void follow(@PathVariable int followedId, Principal principal) {
+//        int followerId = userDAO.findIdByUsername(principal.getName());
+//        followDAO.follow(followerId, followedId);
+//    }
+//
+//    // delete followed user
+//    @RequestMapping(path = "/follow/{followedId}", method = RequestMethod.DELETE)
+//    public void unfollow(@PathVariable int followedId, Principal principal) {
+//        int followerId = userDAO.findIdByUsername(principal.getName());
+//        followDAO.unfollow(followerId, followedId);
+//    }
+//
+//    // get list of followed users
+//    @RequestMapping(path = "/follow", method = RequestMethod.GET)
+//    public List<User> list(Principal principal) {
+//        int userId = userDAO.findIdByUsername(principal.getName());
+//        return followDAO.list(userId);
+//    }
+//}
