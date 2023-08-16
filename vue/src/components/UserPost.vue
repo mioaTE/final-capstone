418025a3d9844c7bb4ad820b1a0586cb0bfbc6f7
Bradlee Bassett <Bradlee1999Bassett@gmail.com>
2023-08-16 14:35:33 -0400

    reverted some changed

--- vue/src/components/UserPost.vue
+++ vue/src/components/UserPost.vue
@@ -16,9 +16,11 @@
 
       </section>
       <section id="InteractionPanel">
-        <button id="likebutton" v-on:click="likePost(post)" v-if="!postLiked" ><i class="fa fa-heart" style="font-size:15px;color:red"></i> Like: {{currentLikes}}</button> 
-        <button id="likebutton" v-on:click="unlikePost(post)" v-if="postLiked" >Unlike: {{currentLikes}}</button>
-        <button id="favoritebutton" v-on:click="favoritePost(post)" v-if="!postFavorited" >Favorite</button>
+
+        <button id="likebutton" v-on:click="likePost(post)" v-if="!postLiked && post.postId != 0" ><i class="fa fa-heart" style="font-size:15px;color:red"></i> Like</button> 
+        <button id="likebutton" v-on:click="unlikePost(post)" v-if="postLiked && post.postId != 0" >Unlike</button>
+         <p v-if="post.postId !=0"> {{this.postLikes}} </p>
+        <button id="favoritebutton" v-on:click="favoritePost(post)" v-if="!postFavorited && post.postId != 0" >Favorite</button>
         <button id="favoritebutton" v-on:click="unFavoritePost(post)" v-if="postFavorited" >Unfavorite</button>
       </section>
       </div>
@@ -83,6 +85,7 @@ export default {
         currentLikes() {
           let likeList = this.allLikes.filter((like) => like.postId == this.post.postId)
           return likeList.length;
+          // return this.post.likesCount;
         }
 
     },
@@ -122,6 +125,8 @@ export default {
                 (like.userId !== this.removeLike.userId && like.postId !== this.removeLike.postId)});
           }
             
+    
+          
         });
         
         
